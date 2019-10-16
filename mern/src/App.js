b2ba8ff8561643612710646f0f5bb75fbee69e0b
Gabriel Sharpton <3bag@Tricias-MBP.lan1>
2019-10-16 11:29:53 -0500

    creating nav

--- mern/src/App.js
+++ mern/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Component} from 'react'
 import './App.css';
 import data from './data/data.json'
+import {Link, Route, Switch} from 'react-router-dom'
 
 export default class App extends Component {
   constructor(props) {
@@ -13,7 +14,17 @@ export default class App extends Component {
   render(){
   return (
     <div className="App">
+      <nav>
+      <Link to="/"><img src="https://img.icons8.com/office/30/000000/reddit.png" alt=""/></Link>
+      </nav>
     <h1> {this.props.data[0].test}</h1>
+    <input type="text"></input>
+    <input type="submit" />
+    <main>
+      <Switch>
+      <Route to="/"/>
+      </Switch>
+    </main>
     </div>
   );
 }
